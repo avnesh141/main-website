16fc5cbb46f2fee0865f9b2e8ab581dbf66f9ef5
avnesh141 <avnesh.kumar.ece21@itbhu.ac.in>
2022-12-25 12:04:57 +0530

    AboutUS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Updates from './components/updates/updates';
+import About from './components/AboutUS/AboutUs';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             element={
               <>
                 <Home />
+                <About />
                 <Updates />
               </>
             }
